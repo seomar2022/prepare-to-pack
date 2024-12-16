98e1b111dd98b3c29c4ddb8773dc2357e8911685
yui <seomar2022@gmail.com>
2024-12-16 20:54:36 +0900

    separate gui from prepare_to_pack

---
+++ gui.py
@@ -0,0 +1,71 @@
+# gui.py
+import tkinter as tk
+from tkinter import PhotoImage, StringVar
+import threading
+from module import *
+
+class GUI:
+    def __init__(self, root, on_before_packing, on_upload_tracking):
+        self.root = root
+        self.on_before_packing = on_before_packing  # 로직 함수 연결
+        self.on_upload_tracking = on_upload_tracking  # 로직 함수 연결
+
+        self.font_size = 14
+        self.log_text = StringVar()  # 로그 텍스트를 관리하는 변수
+        self.log_text.set("")  # 초기값 설정
+
+        self.setup_ui()  # UI 생성
+
+    def setup_ui(self):
+        """GUI를 초기화하는 메서드"""
+        self.root.title("Prepare to Pack")
+        self.root.geometry("350x450")
+        self.root.attributes('-topmost', True)
+
+        #Add margin
+        frame = tk.Frame(self.root, padx=20, pady=20)
+        frame.pack()
+
+        # 타이틀
+        label = tk.Label(self.root, text="출고 준비 프로그램", font=("none", self.font_size, "bold"))
+        label.pack(pady=10) # set title on root
+
+        # 버튼 프레임 생성
+        button_frame = tk.Frame(self.root)
+        button_frame.pack(pady=10)
+
+        # 버튼 이미지
+        before_packing_image = PhotoImage(file="resources/img/package-box.png")
+        upload_image = PhotoImage(file="resources/img/document.png")
+        info_image = PhotoImage(file="resources/img/info.png")
+
+
+        # 포장 준비 버튼
+        before_packing_button = tk.Button(button_frame, image=before_packing_image, command=lambda: threading.Thread(target=self.on_before_packing(self.update_log, self.get_log)).start())
+        before_packing_button.image = before_packing_image
+        before_packing_button.pack(side="left", padx=10)
+        ToolTip(before_packing_button, "cafe24에서 '출고준비통합'양식으로 파일을 다운로드 받은 후 이 버튼을 클릭해 주세요.")
+
+
+        # 송장 업로드 버튼
+        upload_tracking_number_button = tk.Button(button_frame, image=upload_image, command=lambda: threading.Thread(target=self.on_upload_tracking(self.update_log)).start())
+        upload_tracking_number_button.image = upload_image
+        upload_tracking_number_button.pack(side="right", padx=10)
+        ToolTip(upload_tracking_number_button, "한진택배에서 '원본파일'을 다운로드 받은 후 이 버튼을 클릭해 주세요.")
+
+        # info 버튼
+        info_button = tk.Button(self.root, image=info_image)
+        info_button.pack(side="bottom", pady=20)
+        ToolTip(info_button, "-cafe24 엑셀파일 다운 양식 수정: settings\\header.csv\n-인터넷에서 다운 받은 파일이 있는 폴더 경로 지정: settings\\path.csv\n-설명지 추가: \\resources\\product_instruction\n-image: Flaticon.com\n-기타문의:seomar2022@gmail.com")
+
+
+
+        # 로그 레이블
+        log_label = tk.Label(self.root, textvariable=self.log_text, justify="left", anchor="nw", font=self.font_size, wraplength=300)
+        log_label.pack(pady=10, padx=20)
+
+    def update_log(self, message):
+        """로그 텍스트를 업데이트하는 메서드"""
+        self.log_text.set(message)
+    def get_log(self):
+        return self.log_text.get()
--- prepare_to_pack.py
+++ prepare_to_pack.py
@@ -7,11 +7,13 @@ import tkinter as tk #GUI
 import threading #GUI 멀티스레드 사용하기 위해
 import time #GUI에서 멀티스레드 사용하기 위해
 from datetime import datetime #폴더이름에 현재 날짜 넣기 위해
+from gui import GUI
 
-def prepare_to_pack():
+def prepare_to_pack(log_set_callback, log_get_callback):
     try:
+        print("start")
         sleep_time = 0.1
-        log_text.set("시작! 프로그램 실행")
+        log_set_callback("시작! 프로그램 실행")
 
         ####완성된 파일들을 넣어둘 폴더 만들기
         result_directory = "result_" + datetime.now().strftime("%a.%H.%M.%S") #요일.시.분.초
@@ -25,7 +27,7 @@ def prepare_to_pack():
         #카페24에서 다운받는 파일명의 형식: lalapetmall_오늘날짜_일련번호_일련번호
         download_from_cafe24_path = find_path_by_partial_name(download_from_internet_path, "lalapetmall_" + datetime.today().strftime('%Y%m%d') + "_")
         #log
-        log_text.set(log_text.get() + "\n다운로드 받은 파일 검색")
+        log_set_callback(log_get_callback() + "\n다운로드 받은 파일 검색")
         time.sleep(sleep_time) 
 
         ####두 가지 파일로 쪼개기
@@ -42,7 +44,7 @@ def prepare_to_pack():
         split_csv_by_column_index(download_from_cafe24_path, hanjin_path , hanjin_header_index)
         
         #log
-        log_text.set(log_text.get() + "\n헤더명에 따라 두 개의 파일로 분리")
+        log_set_callback(log_get_callback() + "\n헤더명에 따라 두 개의 파일로 분리")
         time.sleep(sleep_time) 
 
         ##########################################print_out_product_instruction##########################################
@@ -53,7 +55,7 @@ def prepare_to_pack():
         # 수정된 내용을 새로운 엑셀 파일로 저장
         order_list_pd.to_excel(rf"{result_directory}\order_list.xlsx", index=False, engine='openpyxl')
         #log
-        log_text.set(log_text.get() + "\n주문리스트의 중량 정보 입력")
+        log_set_callback(log_get_callback() + "\n주문리스트의 중량 정보 입력")
         time.sleep(sleep_time) 
 
         ####설명지 찾아서 병합
@@ -61,26 +63,26 @@ def prepare_to_pack():
         not_found_files = merge_pdf(result_directory, converted_codes)
         report_result(result_directory, order_list_pd, not_found_files)
         #log
-        log_text.set(log_text.get() + "\n상품 설명지 병합")
+        log_set_callback(log_get_callback() + "\n상품 설명지 병합")
         time.sleep(sleep_time) 
 
         ####매크로 실행(포장할 때 참고할 주문리스트 만들기 위해)
         run_macro("전채널주문리스트", order_list_path)
         #log
-        log_text.set(log_text.get() + "\n전채널주문리스트 매크로 실행\n주문리스트 파일 작성")
+        log_set_callback(log_get_callback() + "\n전채널주문리스트 매크로 실행\n주문리스트 파일 작성")
         time.sleep(sleep_time) 
 
         ####카페24 양식에 맞게 수정한 파일 만들기
         match_to_cafe24_example(result_directory, hanjin_path)  
         #log
-        log_text.set(log_text.get() + "\n송장등록을 위한 카페24양식 파일 작성")
+        log_set_callback(log_get_callback() + "\n송장등록을 위한 카페24양식 파일 작성")
         time.sleep(sleep_time)
 
         ####매크로 실행(기존 파일을 한진택배 복수내품 양식에 맞게 변경하기 위해)
         run_macro("ProcessMultipleItems", hanjin_path) 
         os.rename(hanjin_path, rf"{result_directory}\upload_to_hanjin.xlsx")
         #log
-        log_text.set(log_text.get() + "\nProcessMultipleItems 매크로 실행\n한진 사이트에 올릴 파일 작성")
+        log_set_callback(log_get_callback() + "\nProcessMultipleItems 매크로 실행\n한진 사이트에 올릴 파일 작성")
         time.sleep(sleep_time)
 
         ####한진택배 사이트 열기
@@ -90,75 +92,32 @@ def prepare_to_pack():
         os.startfile(f"{result_directory}")
         print("실행 완료.")
         #log
-        log_text.set(log_text.get() + "\n끝! 실행 완료")
+        log_set_callback(log_get_callback() + "\n끝! 실행 완료")
         time.sleep(sleep_time)
     except Exception as e:
-        log_text.set(log_text.get() + f"\n오류 발생: {e}") 
+        log_set_callback(log_get_callback() + f"\n오류 발생: {e}") 
 
-def on_before_packing_button_click():
-    # 별도의 스레드에서 프로그램 로직 실행
-    threading.Thread(target=prepare_to_pack).start()
+# def on_before_packing_button_click():
+#     # 별도의 스레드에서 프로그램 로직 실행
+#     threading.Thread(target=prepare_to_pack).start()
 
-def on_upload_tracking_number_button_click():
+def on_upload_tracking_number_button_click(log_set_callback):
     try:
         upload_tracking_number()
-        log_text.set("실행 완료\n카페24 엑셀 일괄배송 처리란에 업로드해 주세요.")
+        log_set_callback("실행 완료\n카페24 엑셀 일괄배송 처리란에 업로드해 주세요.")
     except Exception as e:
-        log_text.set(f"오류 발생: {e}") 
+        log_set_callback(f"오류 발생: {e}") 
 
 ##########################################GUI##########################################
 #.pack()은 부모위젯 안에 배치
 
 # 메인 윈도우 생성
 if __name__ == "__main__":
+    import tkinter as tk  
+
     root = tk.Tk()
-    root.title("prepare_to_pack")
-    root.geometry("350x450")  # 너비x높이
-    root.configure()
-    root.attributes('-topmost', True) # 창이 포커스를 잃어도 항상 다른 창들보다 위에 표시
 
-
-    font_size = 14
-
-    # 프레임을 사용하여 내부 여백 추가
-    frame = tk.Frame(root, padx=20, pady=20)
-    frame.pack()
-
-    label = tk.Label(root, text="출고 준비 프로그램", font=("none", font_size, "bold"))
-    label.pack(pady=10)  # 여백 설정  # 위젯을 창에 배치
-
-    ####버튼
-    # 버튼 프레임 생성
-    button_frame = tk.Frame(root)
-    button_frame.pack(pady=10)
-
-    #버튼 이미지
-    before_packing_image = tk.PhotoImage(file="resources/img/package-box.png")
-    upload_image = tk.PhotoImage(file="resources/img/document.png")
-    info_image = tk.PhotoImage(file="resources/img/info.png")
-
-    # 포장 준비 버튼
-    before_packing_button = tk.Button(button_frame, image=before_packing_image, command=on_before_packing_button_click)
-    before_packing_button.pack(side="left", padx=10)
-    ToolTip(before_packing_button, "cafe24에서 '출고준비통합'양식으로 파일을 다운로드 받은 후 이 버튼을 클릭해 주세요.")
-
-    # 송장 업로드 버튼
-    upload_tracking_number_button = tk.Button(button_frame, image=upload_image, command=on_upload_tracking_number_button_click, font=font_size)
-    upload_tracking_number_button.pack(side="right", padx=10)
-    ToolTip(upload_tracking_number_button, "한진택배에서 '원본파일'을 다운로드 받은 후 이 버튼을 클릭해 주세요.")
-
-    # info 버튼
-    info_button = tk.Button(root, image=info_image)
-    info_button.pack(side="bottom", pady=20)
-    ToolTip(info_button, "-cafe24 엑셀파일 다운 양식 수정: \settings\header.csv\n-인터넷에서 다운 받은 파일이 있는 폴더 경로 지정: \settings\path.csv\n-설명지 추가: \\resources\product_instruction\n-image: Flaticon.com\n-기타문의:seomar2022@gmail.com")
-
-
-    #### 로그 텍스트를 표시할 라벨 생성
-    log_text = tk.StringVar()
-    log_text.set("")
-
-    log_label = tk.Label(root, textvariable=log_text, justify="left", anchor="nw", font=font_size, wraplength=300)
-    log_label.pack(pady=10, padx=20)
+    gui = GUI(root, prepare_to_pack, on_upload_tracking_number_button_click)
 
     # 메인 루프 시작
     root.mainloop()
